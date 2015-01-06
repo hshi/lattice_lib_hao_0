6fcb276fdbfd648a7cf67f7fde8be5cfe87e0330
boruoshihao <boruoshihao@gmail.com>
2015-01-05 22:51:11 -0500

    change indentation to four.

--- test/test_all.cpp
+++ test/test_all.cpp
@@ -6,21 +6,21 @@ void supercubic_test();
 
 int main(int argc, char** argv)
 {
- int rank=0;
+    int rank=0;
 
 #ifdef MPI_HAO
- MPI_Init(&argc,&argv);
- MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 #endif
 
- if(rank==0)
- {
-  supercubic_test();
- }
+    if(rank==0)
+    {
+        supercubic_test();
+    }
 
 #ifdef MPI_HAO
- MPI_Finalize();
+    MPI_Finalize();
 #endif
 
- return 0;
+    return 0;
 }
